039ea44e58a094f745a3555a67d69f6ffadbb098
luizg <luizg@Otvio>
2024-09-30 13:57:47 -0300

    teste commit inicial

---
+++ src/test/java/backend/develoment/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.develoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
